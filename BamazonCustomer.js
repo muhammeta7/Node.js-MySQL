97e5688ec0d89dc987395bd591c928ca877c1047
Muhammet <muhammeta7@gmail.com>
2016-10-21 12:50:30 -0400

    BamazonCustomer.js complete without read prompt

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -11,64 +11,95 @@ var connection = mysql.createConnection({
 });
 
 connection.connect(function(err) {
-    if (err) throw err;
-    // console.log("connected as id " + connection.threadId);
+  if (err) throw err;
+  // console.log("connected as id " + connection.threadId);
 });
 
 // Show Table with all products and displays them
-connection.query('SELECT * FROM products', function(err,res){
-  for ( var i = 0; i < res.length; i++){
-    console.log( 'ID: ' + res[i].ItemID + " | Name: " + res[i].ProductName + " | Department: " + res[i].DepartmentName + " | $"  + res[i].Price + ' | Available: ' + res[i].StockQuantity);
-  }
-  console.log('---------------------------------------------------------------------------');
-  start();
-});
-
-
-// Start node app
-var start = function() {
-  inquirer.prompt({
-    type: 'confirm',
-    name: 'makePurchase',
-    message: 'Would you like to purchase an item from Bamazon?'
-  }).then(function(answer) {
-    console.log(answer.makePurchase)
-    if (answer.makePurchase == true){
-      pickByItemID();
-    } else{
-      console.log('Have a great day! Come shop with us on payday!')
+function start(){
+  connection.query('SELECT * FROM products', function(err,res){
+    for ( var i = 0; i < res.length; i++){
+      console.log( 'ID: ' + res[i].ItemID + " | Product: " + res[i].ProductName + " | Department: " + res[i].DepartmentName + " | $"  + res[i].Price + ' | Available: ' + res[i].StockQuantity);
     }
+    console.log('---------------------------------------------------------------------------');
+    // Start node app
+    inquirer.prompt({
+      type: 'confirm',
+      name: 'makePurchase',
+      message: 'Welcome to Bamazon, would you like to purchase an item?'
+    }).then(function(answer) {
+      if (answer.makePurchase){
+        console.log('--------------------------------------------------------')
+        inquirer.prompt([
+          {
+            type: 'input',
+            name: 'itemID',
+            message: 'Type the ID of the item you would like to buy.',
+            validate: function(value) {
+                    if (isNaN(value) == false) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+          },
+          {
+          type: 'input',
+          name: 'quantity',
+          message: 'How many of this item would you like to purchase?',
+          validate: function(value) {
+                  if (isNaN(value) == false) {
+                      return true;
+                  } else {
+                      return false;
+                  }
+              } 
+
+          }
+          ]).then(function(answer){
+            var itemSelected = (answer.itemID)-1;
+            var quantity = parseInt(answer.quantity);
+            var cartTotal = (res[itemSelected].Price) * quantity;
+            // Check inventory to see if there is enough stock available
+            if (res[itemSelected].StockQuantity >= quantity){
+              console.log('You have successfully made your purchase. Your total is $' + cartTotal + '.00. Your item(s) will be shipped within 2 business days!');
+              // After purchases have been made update database
+              connection.query('UPDATE products SET ? WHERE ?', [
+                { StockQuantity: (res[itemSelected].StockQuantity-quantity)},
+                { ItemID: answer.itemID}
+                ], function(err, res){
+                  if(err) throw err;
+                });
+              } else{
+                console.log('Sorry, we do not have enough of that item in our inventory to finalize your purchase.')
+              }
+           });
+      } else{
+        console.log('Have a great day! Come shop with us on payday!');
+      }
+    })
+  
   })
 }
 
-// User selects item by ID
-// function pickByItemID(){
-//   inquirer.prompt(
-//   {
-//     var questions = [
-//     {
-//     type: 'input',
-//     name: 'itemID',
-//     message: 'Welcome to Bamazon. Type the Item ID that you would like to add to your cart.'
-//     },
-//     {
 
+// function reprompt(){
+//   iquirer.prompt([{
+//     type: 'confirm',
+//     name: 'response',
+//     message: 'Would you like to purchase another item?'
+//   }]).then(function(answer){
+//     if (answer.response){
+//       start();
+//     } else {
+//       console.log('Have a great die!')
 //     }
-//     ]
-//     type: 'input',
-//     name: 'itemID',
-//     message: 'Welcome to Bamazon. Type the Item ID that you would like to add to your cart.'
-//   }.then(function(answer){
-//     if ( answer.itemID == )
-//   }
+//   });
 // }
-//    if ( answer.firstChoice == 'Post an Item'){
-//       var questions = [
-//       {
-//         type: 'input',
-//         name: 'itemName',
-//         message: 'What is the item?'
-//       },
+
+start();
+
+
 
 // Populate Products table with ten random items
 // connection.query('INSERT INTO products SET ?',{
@@ -164,65 +195,6 @@ var start = function() {
 
 
 
-
-
-// var start = function(){
-//   inquirer.prompt({
-//     name: ''
-//   }) 
-// }
-
-// var start = function() {
-//     inquirer.prompt({
-//         name: "postOrBid",
-//         type: "rawlist",
-//         message: "Welcome to Bamazon",
-//         choices: ["POST", "BID"]
-//     }).then(function(answer) {
-//         if (answer.postOrBid.toUpperCase() == "POST") {
-//             postAuction();
-//         } else {
-//             bidAuction();
-//         }
-//     })
-// }
-
-// var postAuction = function() {
-//     inquirer.prompt([{
-//         name: "item",
-//         type: "input",
-//         message: "What is the item you would like to submit?"
-//     }, {
-//         name: "category",
-//         type: "input",
-//         message: "What category would you like to place your auction in?"
-//     }, {
-//         name: "startingBid",
-//         type: "input",
-//         message: "What would you like your starting bid to be?",
-//         validate: function(value) {
-//             if (isNaN(value) == false) {
-//                 return true;
-//             } else {
-//                 return false;
-//             }
-//         }
-//     }]).then(function(answer) {
-//         connection.query("INSERT INTO auctions SET ?", {
-//             itemname: answer.item,
-//             category: answer.category,
-//             startingbid: answer.startingBid,
-//             highestbid: answer.startingBid
-//         }, function(err, res) {
-//             console.log("Your auction was created successfully!");
-//             start();
-//         });
-//     })
-// }
-
-
-
-
 // SQL Commands
 // CREATE DATABASE Bamazon;
 // USE Bamazon;
@@ -234,3 +206,6 @@ var start = function() {
 //   StockQuantity INT default 0,
 //   PRIMARY KEY (id)
 // );
+
+
+
